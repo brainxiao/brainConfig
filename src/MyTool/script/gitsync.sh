776f5591fa78a810b6606cd833b886bd9c566210
xiaozhitao <xiaozhitao@cvte.com>
2019-09-27 10:29:19 +0800

    fix gitsync script

--- src/MyTool/script/gitsync.sh
+++ src/MyTool/script/gitsync.sh
@@ -2,8 +2,10 @@
 ### author:brian
 ### date:19.09.27
 ### use this script when you push code conflit
-
+echo "========reset start========"
 git reset --hard HEAD^^^^
+echo "========pull start========="
 git pull
+echo "========cherry-pick start========"
 git cherry-pick $1
 
